83d36bcb2b1a12ffd84b76ca70bfe9f4b46b2a2f
rishabh06704 <rishabh06704@gmail.com>
2024-04-16 22:57:21 -0400

    week2

---
+++ week2/App.js
@@ -0,0 +1,26 @@
+import React from React;
+import {text, view, image, Button, FlatList} from 'react-native';
+
+const Dog = props => {
+  return (
+    <view> 
+      <image source =(require)
+    </view>
+  )
+
+};
+
+const Doglist = () => {
+  return {
+    <view>
+      <flatlist>
+        data=({
+          (name; "John")
+          
+        })
+      </flatlist>
+
+  }
+}
+
+export default Dog;
