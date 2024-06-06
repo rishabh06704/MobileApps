23032c6675b63081ff61ffbd3b6f7590597ccad5
rishabh06704 <rishabh06704@gmail.com>
2024-06-06 19:06:18 -0400

    syncc

--- tux/assignment4_info670/retrievemessage.php
+++ tux/assignment4_info670/retrievemessage.php
@@ -3,13 +3,13 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   header('Access-Control-Allow-Origin: http://localhost:8081');
-  header('Access-Control-Allow-Methods: GET, POST, FETCH, OPTIONS');
+  header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
   header('Access-Control-Allow-Headers: Content-Type');
   exit(0);
 }
 
 header('Access-Control-Allow-Origin: http://localhost:8081');
-header('Access-Control-Allow-Methods: GET, POST,FETCH, OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 $recipient = $_GET['recipient'];
